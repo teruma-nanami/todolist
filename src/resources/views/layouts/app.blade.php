92f59de7af083a9c17ff4a4e7dd0d28e7c6bfc8d
teruma-nanami <kinos.travel.fun@gmail.com>
2024-05-27 10:18:03 +0900

    finish blade&css

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>TodoList</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+  @yield('css')
 </head>
 <body>
-  
+  <header class="header">
+    <div class="header__inner">
+      <a href="" class="header__logo">Todo</a>
+    </div>
+  </header>
+
+  <main>
+    @yield('content')
+  </main>
 </body>
 </html>
